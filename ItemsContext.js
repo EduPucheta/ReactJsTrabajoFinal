de23979c2f68046f062ec18ed0eb0ae19f3521eb
EduPucheta <43687004+EduPucheta@users.noreply.github.com>
2023-03-07 23:37:54 -0300

    Add files via upload

---
+++ ItemsContext.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { useState, createContext } from "react";
+
+export const ItemsContext = createContext();
+
+const ItemsinStock = [];
+
+export const ItemsProvider = ({ children }) => {
+  const [items, setItems] = useState(ItemsinStock);
+
+  //FUNCION PARA AGREGAR UN ITEM
+
+
+  const addToCart = (item, newQuantity) => {
+    const newCart = items.filter((prod) => prod.id !== item.id);
+    newCart.push({ ...item, quantity: newQuantity });
+    setItems(newCart);
+  };
+
+  //TOTAL DE LA COMPRA
+
+  const totalCompra = (x) => {
+    let total = 0;
+    x.map((e) => {
+      let monto = e.quantity * e.price;
+      return total += monto;
+    });
+    return total;
+  };
+
+  return (
+    <ItemsContext.Provider value={{ items, setItems, addToCart, totalCompra }}>
+      {children}
+    </ItemsContext.Provider>
+  );
+};
